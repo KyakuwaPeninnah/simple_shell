a3aa1408d353e263b53db2f41c3a932ad44998a8
KyakuwaPeninnah <kyakuwapeninnah@gmail.com>
2023-09-22 16:02:02 -0700

    fixed betty checks

--- convert_funcs.c
+++ convert_funcs.c
@@ -20,42 +20,42 @@
  */
 long int _atoi(char *s)
 {
-  int i, len, f, digit;
-  long int n;
+	int i, len, f, digit;
+	long int n;
 
-  i = 0;
-  n = 0;
-  len = _strlen(s);
-  f = 0;
-  digit = 0;
+	i = 0;
+	n = 0;
+	len = _strlen(s);
+	f = 0;
+	digit = 0;
 
-  if (s[i] == '+')
-    i++;
+	if (s[i] == '+')
+		i++;
 
-  while (i < len && f == 0)
-    {
-      if (s[i] == '-')
-	return (-1);
-
-      if (s[i] >= '0' && s[i] <= '9')
+	while (i < len && f == 0)
 	{
-	  digit = s[i] - '0';
-	  n = n * 10 + digit;
-	  f = 1;
-	  if (s[i + 1] < '0' || s[i + 1] > '9')
-	    break;
-	  f = 0;
+		if (s[i] == '-')
+			return (-1);
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-      i++;
-    }
 
-  if (f == 0)
-    return (0);
+	if (f == 0)
+		return (0);
 
-  if (n > INT_MAX || n < 0)
-    return (-1);
+	if (n > INT_MAX || n < 0)
+		return (-1);
 
-  return (n);
+	return (n);
 }
 
 /**
@@ -73,16 +73,16 @@ long int _atoi(char *s)
  */
 char *convert(int num, int base)
 {
-  static char *rep = "0123456789";
-  static char buffer[50];
-  char *ptr = NULL;
+	static char *rep = "0123456789";
+	static char buffer[50];
+	char *ptr = NULL;
 
-  ptr = &buffer[49];
-  *ptr = '\0';
-  do {
-    *--ptr = rep[num % base];
-    num /= base;
-  } while (num != 0);
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
 
-  return (ptr);
+	return (ptr);
 }
